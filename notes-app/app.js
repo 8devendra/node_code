6096e08cdb4e16b9b26fd2242e600a5f5eed20ff
Devendra Haldankar <devendra6969@gmail.com>
2021-03-24 23:45:24 +0530

    init

---
+++ notes-app/app.js
@@ -0,0 +1,11 @@
+//const validator=require('validator')
+const chalk=require('chalk')
+//const nm = require('./util.js')
+//const note=require('./notes')
+
+//const name='Devendra'
+//console.log(nm(10,20))
+//console.log(note())
+
+//console.log(validator.isEmail('devednra6969@gmail.com'))
+console.log(chalk.black.bgGreen('Success..'))
---
+++ notes-app/notes.js
@@ -0,0 +1,8 @@
+console.log('notes.js')
+
+
+const notes=function () {
+    const b='Your Notes..'
+    return b    
+}
+module.exports=notes
