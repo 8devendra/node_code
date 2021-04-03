e59c06df723f5f911580e3459d063811c2e0c5fb
Devendra Haldankar <devendra6969@gmail.com>
2021-04-03 11:33:25 +0530

    Learn about Asynchronous Basic

--- notes-app/weather-app/app.js
+++ notes-app/weather-app/app.js
@@ -3,6 +3,9 @@ setTimeout(()=>{
     console.log("2 sec Timeout")
 },2000)
 setTimeout(()=>{
+    console.log("1 sec Timeout")
+},1000)
+setTimeout(()=>{
     console.log("0 ms Timeout")
 },0)
 console.log("Stoping")
