f5192563bd0f25f9ec5f9a4b48ab8fd7326d3fde
Devendra Haldankar <devendra6969@gmail.com>
2021-04-04 17:07:09 +0530

    Trying to connect using api key beginning

--- notes-app/weather-app/app.js
+++ notes-app/weather-app/app.js
@@ -1,11 +1,24 @@
-console.log("Starting")
-setTimeout(()=>{
-    console.log("2 sec Timeout")
-},2000)
-setTimeout(()=>{
-    console.log("1 sec Timeout")
-},1000)
-setTimeout(()=>{
-    console.log("0 ms Timeout")
-},0)
-console.log("Stoping")
+// console.log("Starting")
+// setTimeout(()=>{
+//     console.log("2 sec Timeout")
+// },2000)
+// setTimeout(()=>{
+//     console.log("1 sec Timeout")
+// },1000)
+// setTimeout(()=>{
+//     console.log("0 ms Timeout")
+// },0)
+// console.log("Stoping")
+
+
+const request=require('request')
+const key=''
+
+const url ="http://api.weatherstack.com/current?access_key="+key+"&query=15.918875,%2073.814398"
+
+request({url:url},(error,response)=>{
+    const data=JSON.parse(response.body)
+
+    console.log(data.current)
+
+})
